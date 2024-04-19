87622eb781f63757c637e0899c180319afa75286
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-04-19 10:11:43 +0200

    User class added

---
+++ src/main/java/FabioGilardi/U5W3D5/entities/User.java
@@ -0,0 +1,65 @@
+package FabioGilardi.U5W3D5.entities;
+
+import FabioGilardi.U5W3D5.enums.UserAuthority;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties({"password", "role", "authorities", "accountNonExpired", "accountNonLocked", "credentialsNonExpired", "enabled"})
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+
+    private String name, surname, email, username, password;
+
+    @Enumerated(EnumType.STRING)
+    private UserAuthority role;
+
+    public User(String name, String surname, String email, String username, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
---
+++ src/main/java/FabioGilardi/U5W3D5/enums/UserAuthority.java
@@ -0,0 +1,5 @@
+package FabioGilardi.U5W3D5.enums;
+
+public enum UserAuthority {
+    USER, EVENT_ORGANIZER
+}
